8f8966ffca07c4d1c8b1e4d6547eb31c2604eab1
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-02-23 08:24:49 -0500

    Server que usa goroutine por cada conexion

--- 01-exercise/01-goroutines/02-client-server/server/main.go
+++ 01-exercise/01-goroutines/02-client-server/server/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
 )
 
 func main() {
 	// TODO: write server program to handle concurrent client connections.
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			continue
+		}
+
+		// handleConn(conn) // asi podemos tener una sola conexion
+		go handleConn(conn) // con este creara una goroutine para cada conexion
+	}
 }
 
 // handleConn - utility function
